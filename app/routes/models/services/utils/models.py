f3d8a9016e30e008da175bca0be98cc1db903a23
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-05-13 03:58:39 -0500

    v1.3 Se modificó el DTO para que sea igual a la API Usuarios

--- app/routes/models/services/utils/models.py
+++ app/routes/models/services/utils/models.py
@@ -1,24 +1,20 @@
+from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import List
 
 class ProductoCompra(BaseModel):
-    id: int = Field(..., example=1, description="ID del producto")
-    cantidad: int = Field(..., example=2, description="Cantidad del producto a comprar")
+    id: int = Field(..., description="ID del producto")
+    cantidad: int = Field(..., description="Cantidad del producto a comprar")
 
 class Compra(BaseModel):
-    usuario_id: int = Field(..., example=12345, description="ID del usuario que realiza la compra")
-    productos: List[ProductoCompra] = Field(..., description="Lista de productos a comprar")
-    fecha: str = Field(
-        ...,
-        example="2025-05-11T12:00:00",
-        pattern="^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$",
-        description="Fecha de la compra en formato ISO")
-    
+    usuario_id: int = Field(..., example=20007)
+    productos: List[ProductoCompra]
+    fecha: datetime
 class ProductoFactura(BaseModel):
-    id_prod: int = Field(..., example=2293, description="ID del producto")
-    precio_unitario: float = Field(..., example=1335.48, description="Precio unitario del producto")
-    cantidad: int = Field(..., example=1, description="Cantidad del producto")
-    subtotal: float = Field(..., example=1335, description="Subtotal del producto")
+    id_prod: int = Field(..., description="ID del producto")
+    precio_unitario: float = Field(..., description="Precio unitario del producto")
+    cantidad: int = Field(..., description="Cantidad del producto")
+    subtotal: float = Field(..., description="Subtotal del producto")
 
 class UsuarioInfo(BaseModel):
     nombre: str = Field(..., example="Yago")
